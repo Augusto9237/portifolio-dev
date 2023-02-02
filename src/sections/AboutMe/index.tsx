badb4698743b019b3e868a9b9c985a9e3efd712e
Augusto9237 <79643279+Augusto9237@users.noreply.github.com>
2023-02-02 17:22:34 -0300

    2 files modified

--- src/sections/AboutMe/index.tsx
+++ src/sections/AboutMe/index.tsx
@@ -1,13 +1,35 @@
-
 import { BsCodeSlash } from 'react-icons/bs';
-import { Content, CircleImg } from '../AboutMe/styles';
+import { AboutMeDescription, CircleImg, Content } from '../AboutMe/styles';
+import { Greetings } from '../Home/styles';
 
 export function AboutMe() {
   return (
-   <Content>
-     <CircleImg>
-
-     </CircleImg>
-   </Content>
+    <Content>
+      <CircleImg />
+      <AboutMeDescription>
+        <Greetings>🧐 Sobre mim</Greetings>
+        <p>
+          <code>
+            👋 Me chamo Washington Henrique Fernandes de Sousa, mas pode me
+            chamar apenas de Henrique. Prazer!
+          </code>
+          <code>
+            👨‍💻 Há mais de 2 anos desenvolvendo e programando interfaces com
+            JavaScript, React JS e Typescript.
+          </code>
+          <code>
+            🎓 Graduado em Análise e Desenvolvimento de Sistemas pela Fatec de
+            São José dos Campos.
+          </code>
+          <code>
+            💡 Interesses em desenvolvimento Front-end com React, React Native,
+            VueJS e UX/UI Design.
+          </code>
+          <code>
+            🚀 Tentando ser um pouquinho melhor do que ontem todos dias.
+          </code>
+        </p>
+      </AboutMeDescription>
+    </Content>
   );
 }
--- src/sections/AboutMe/styles.ts
+++ src/sections/AboutMe/styles.ts
@@ -1,15 +1,29 @@
 import styled from 'styled-components';
 
 export const Content = styled.section`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-gap: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 60px;
 `;
 
 export const CircleImg = styled.div`
-background-color: #ffff;
-height: 460px;
-width: 460px;
-border-radius: 50%;
+  background-color: #ffff;
+  height: 460px;
+  width: 460px;
+  border-radius: 50%;
+`;
+
+export const AboutMeDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 920px;
+
+  p {
+    display: flex;
+    flex-direction: column;
+    text-align: justify;
+    color: rgba(255, 255, 255, 0.5);
+    word-break: break-all;
+  }
 `;
